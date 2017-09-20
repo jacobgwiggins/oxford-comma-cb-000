35816c91c999462b87d9603e28839997b90b20be
jacobgwiggins <jacobgwiggins9@gmail.com>
2017-09-20 22:24:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,12 @@ def oxford_comma(array)
 when array.size == 1
   array.join
 when  array.size == 2
-  array.join(" and ")
+  array = array.join(" and ")
+when array.size > 2
+  popped = array.pop
+  array = array.join(",")
+  array = array + " and " + popped
+  
+  
 end
+
